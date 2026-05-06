3ab5562c2fb8399042461df8d05f208c3fb4d8e3
yoonsun9128 <113073475+yoonsun9128@users.noreply.github.com>
2026-05-06 23:11:50 +0900

    t2 red

--- src/test/java/com/back/CalcTest.java
+++ src/test/java/com/back/CalcTest.java
@@ -12,12 +12,12 @@ public class CalcTest {
 		assertThat(Calc.run("1 + 1")).isEqualTo(2);
 	}
 
-//	@Test
-//	@DisplayName("2 + 1 == 3")
-//	void t2() {
-//		assertThat(Calc.run("2 + 1")).isEqualTo(3);
-//	}
-//
+	@Test
+	@DisplayName("2 + 1 == 3")
+	void t2() {
+		assertThat(Calc.run("2 + 1")).isEqualTo(3);
+	}
+
 //	@Test
 //	@DisplayName("2 + 2 == 4")
 //	void t3() {
