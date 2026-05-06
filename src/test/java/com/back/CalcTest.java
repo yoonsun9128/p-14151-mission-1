907a5bebdc094e01425fc78d706ee799576e908a
yoonsun9128 <113073475+yoonsun9128@users.noreply.github.com>
2026-05-06 23:35:39 +0900

    t9 red

--- src/test/java/com/back/CalcTest.java
+++ src/test/java/com/back/CalcTest.java
@@ -54,12 +54,12 @@ public class CalcTest {
 		assertThat(Calc.run("10 + 20 + 30")).isEqualTo(60);
 	}
 
-//	@Test
-//	@DisplayName("10 - 20 + 30 == 20")
-//	void t9() {
-//		assertThat(Calc.run("10 - 20 + 30")).isEqualTo(20);
-//	}
-//
+	@Test
+	@DisplayName("10 - 20 + 30 == 20")
+	void t9() {
+		assertThat(Calc.run("10 - 20 + 30")).isEqualTo(20);
+	}
+
 //	@Test
 //	@DisplayName("10 - 10 - 10 - 10 == -20")
 //	void t10() {
