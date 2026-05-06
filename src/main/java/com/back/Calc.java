18391c68e86946d7229904d70a07e2e659b70cc8
yoonsun9128 <113073475+yoonsun9128@users.noreply.github.com>
2026-05-06 23:33:31 +0900

    t8 blue

--- src/main/java/com/back/Calc.java
+++ src/main/java/com/back/Calc.java
@@ -4,17 +4,22 @@ public class Calc {
 	public static int run(String cal) {
 		String[] parts;
 		boolean type = true;
+		int result = 0;
 		if (cal.contains("+")) {
 			parts = cal.split(" \\+ ");
 		} else {
 			parts = cal.split(" \\- ");
 			type = false;
 		}
-		int num1 = Integer.parseInt(parts[0]);
-		int num2 = Integer.parseInt(parts[1]);
 		if (type) {
-			return num1 + num2;
+			for (String part :parts) {
+				int num1 = Integer.parseInt(part);
+				result+=num1;
+			}
+			return result;
 		} else {
+			int num1 = Integer.parseInt(parts[0]);
+			int num2 = Integer.parseInt(parts[1]);
 			return num1 - num2;
 		}
 	}
