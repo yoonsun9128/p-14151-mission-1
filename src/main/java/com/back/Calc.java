b4efec7d3414d26b9aca32906d67c0c59b65841f
yoonsun9128 <113073475+yoonsun9128@users.noreply.github.com>
2026-05-06 23:23:07 +0900

    t3 blue

--- src/main/java/com/back/Calc.java
+++ src/main/java/com/back/Calc.java
@@ -2,11 +2,9 @@ package com.back;
 
 public class Calc {
 	public static int run(String cal) {
-		if (cal.equals("2 + 1")) {
-			return 3;
-		} else if (cal.equals("2 + 2")) {
-			return 4;
-		}
-		return 2;
+		String[] parts = cal.split(" \\+ ");
+		int num1 = Integer.parseInt(parts[0]);
+		int num2 = Integer.parseInt(parts[1]);
+		return num1 + num2;
 	}
 }
